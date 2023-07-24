d09ee8fb6c70c26be9f739290606bcc2387ba122
David <osodavid001@gmail.com>
2023-07-24 15:46:32 +0100

    added teacher login method

--- Testing/src/main/java/com/test/Testing/controller/TeacherController.java
+++ Testing/src/main/java/com/test/Testing/controller/TeacherController.java
@@ -1,4 +1,23 @@
 package com.test.Testing.controller;
 
+import com.test.Testing.data.dto.request.TeacherLoginRequest;
+import com.test.Testing.service.teacher.TeacherService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/testing/teacher/")
 public class TeacherController {
+    private final TeacherService teacherService;
+
+    @PostMapping("login")
+    public ResponseEntity<?> teacherLogin(TeacherLoginRequest teacherLoginRequest){
+        String response = teacherService.login(teacherLoginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
