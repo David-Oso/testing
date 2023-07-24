e1511d2167df168bf0750b36b70ad244d87c5b20
David <osodavid001@gmail.com>
2023-07-24 13:07:17 +0100

    get teacher by email method working

--- Testing/src/test/java/com/test/Testing/service/teacher/TeacherServiceImplTest.java
+++ Testing/src/test/java/com/test/Testing/service/teacher/TeacherServiceImplTest.java
@@ -1,12 +1,14 @@
 package com.test.Testing.service.teacher;
 
 import com.test.Testing.data.dto.request.TeacherLoginRequest;
+import com.test.Testing.data.model.AppUser;
+import com.test.Testing.data.model.Teacher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class TeacherServiceImplTest {
     @Autowired TeacherService teacherService;
@@ -22,5 +24,9 @@ class TeacherServiceImplTest {
 
     @Test
     void getTeacherByEmail() {
+        Teacher teacher = teacherService.getTeacherByEmail("noreply@testing.com");
+        AppUser appUser = teacher.getAppUser();
+        assertThat(appUser.getFirstName()).isEqualTo("teacher");
+        assertThat(appUser.getLastName()).isEqualTo("teacher");
     }
 }
