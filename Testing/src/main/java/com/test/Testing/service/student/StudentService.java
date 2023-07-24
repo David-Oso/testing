129286244687acf68f3be9edc6aa2edca832839f
David <osodavid001@gmail.com>
2023-07-24 11:33:39 +0100

    new commit

--- Testing/src/main/java/com/test/Testing/service/student/StudentService.java
+++ Testing/src/main/java/com/test/Testing/service/student/StudentService.java
@@ -1,9 +1,17 @@
 package com.test.Testing.service.student;
 
+import com.test.Testing.data.dto.request.LoginRequest;
 import com.test.Testing.data.dto.request.RegisterStudentRequest;
+import com.test.Testing.data.model.Student;
+
+import java.util.List;
 
 public interface StudentService {
     String registerStudent(RegisterStudentRequest registerStudentRequest);
-    String login();
-
+    String login(LoginRequest loginRequest);
+    String writeToAStudent(Long firstStudentId, Long secondStudentId);
+    Student getStudentById(Long studentId);
+    List<Student> getAllStudents();
+    void deleteAStudent(Long studentId);
+    void deleteAllStudents();
 }
