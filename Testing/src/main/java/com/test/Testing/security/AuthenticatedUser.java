4cdb1909a16d084f994b104d7fa0924e02c171f5
David <osodavid001@gmail.com>
2023-07-25 15:32:53 +0100

    added security

---
+++ Testing/src/main/java/com/test/Testing/security/AuthenticatedUser.java
@@ -0,0 +1,57 @@
+package com.test.Testing.security;
+
+import com.test.Testing.data.model.AppUser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthenticatedUser implements UserDetails {
+    private AppUser appUser;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String roleName = appUser.getRole().name();
+        return List.of(new SimpleGrantedAuthority(roleName));
+    }
+
+    @Override
+    public String getPassword() {
+        return appUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return appUser.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
