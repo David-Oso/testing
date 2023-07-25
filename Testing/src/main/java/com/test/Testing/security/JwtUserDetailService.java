39de2d16f2724d567572197122b93224d273d9a2
David <osodavid001@gmail.com>
2023-07-25 16:49:06 +0100

    added app user repository

--- Testing/src/main/java/com/test/Testing/security/JwtUserDetailService.java
+++ Testing/src/main/java/com/test/Testing/security/JwtUserDetailService.java
@@ -1,4 +1,9 @@
 package com.test.Testing.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class JwtUserDetailService {
 }
