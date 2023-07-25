00b9d45d85fbc8872513143b5a756bab67eb914d
David <osodavid001@gmail.com>
2023-07-25 16:39:49 +0100

    validate jwt token

--- Testing/src/main/java/com/test/Testing/security/JwtUtils.java
+++ Testing/src/main/java/com/test/Testing/security/JwtUtils.java
@@ -66,9 +66,35 @@ public class JwtUtils {
                 .compact();
     }
 
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64URL
                 .decode(jwtSecretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
+
+//    public Boolean validateToken(String token) {
+//        try {
+//            Jwts.parser()
+//                    .setSigningKey(jwtSecretKey)
+//                    .parseClaimsJws(token);
+//            return true; // Token is signed
+//        } catch (SignatureException e) {
+//            return false; //unsigned token
+//        } catch (JwtException e) {
+//            return false; // invalid token
+//        }
+//    }
