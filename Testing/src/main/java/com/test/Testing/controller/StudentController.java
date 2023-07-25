4aef1d65ba24285009cd0ec8534b1fb5049d79b2
David <osodavid001@gmail.com>
2023-07-25 14:35:26 +0100

    student controller delete all students

--- Testing/src/main/java/com/test/Testing/controller/StudentController.java
+++ Testing/src/main/java/com/test/Testing/controller/StudentController.java
@@ -51,7 +51,7 @@ public class StudentController {
     public ResponseEntity<?> deleteStudentById(@Valid@PathVariable Long id){
         studentService.deleteStudentById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Student deleted.");
-    }f
+    }
     @DeleteMapping("all/delete")
     public ResponseEntity<?> deleteAllStudents(){
         studentService.deleteAllStudents();
