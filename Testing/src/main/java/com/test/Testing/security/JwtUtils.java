11b681011883f67835c7f13f011ac84b923f525d
David <osodavid001@gmail.com>
2023-07-25 16:22:20 +0100

    added to jwt utils class

--- Testing/src/main/java/com/test/Testing/security/JwtUtils.java
+++ Testing/src/main/java/com/test/Testing/security/JwtUtils.java
@@ -1,7 +1,45 @@
 package com.test.Testing.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Component
 public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+    @Value("${access.token.expiration}")
+    private Long accessTokenExpiration;
+    @Value("${refresh.token.expiration}")
+    private Long refreshTokenExpiration;
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64URL
+                .decode(jwtSecretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
