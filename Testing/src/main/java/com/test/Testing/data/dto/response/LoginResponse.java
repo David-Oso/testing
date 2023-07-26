17aa851cd87aa3bcc9021444d68948f92e392e5b
David <osodavid001@gmail.com>
2023-07-26 15:44:15 +0100

    created login response

---
+++ Testing/src/main/java/com/test/Testing/data/dto/response/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.test.Testing.data.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginResponse {
+    private String message;
+    private boolean isSuccess;
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
--- Testing/src/main/java/com/test/Testing/service/student/StudentService.java
+++ Testing/src/main/java/com/test/Testing/service/student/StudentService.java
@@ -2,13 +2,15 @@ package com.test.Testing.service.student;
 
 import com.test.Testing.data.dto.request.LoginRequest;
 import com.test.Testing.data.dto.request.RegisterStudentRequest;
+import com.test.Testing.data.dto.response.LoginResponse;
+import com.test.Testing.data.dto.response.RegisterResponse;
 import com.test.Testing.data.model.Student;
 
 import java.util.List;
 
 public interface StudentService {
-    String registerStudent(RegisterStudentRequest registerStudentRequest);
-    String login(LoginRequest loginRequest);
+    RegisterResponse registerStudent(RegisterStudentRequest registerStudentRequest);
+    LoginResponse login(LoginRequest loginRequest);
     Student getStudentById(Long studentId);
     Student getStudentByEmail(String email);
     List<Student> getAllStudents();
