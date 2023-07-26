14a69c3d678c5602dd2b66fbc138edd0a0109781
David <osodavid001@gmail.com>
2023-07-26 15:43:00 +0100

    created register response

--- Testing/src/main/java/com/test/Testing/security/LogoutService.java
+++ Testing/src/main/java/com/test/Testing/security/LogoutService.java
@@ -1,19 +1,28 @@
 package com.test.Testing.security;
 
+import com.test.Testing.security.jwtToken.JwtTokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
+    private final JwtTokenRepository jwtTokenRepository;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
+        String token = extractTokenFromRequest(request);
+        var storedToken = jwtTokenRepository.findByJwtToken(token).orElse(null);
+        if(storedToken != null){
+            storedToken.setExpired(true);
+            jwtTokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
     }
     private String extractTokenFromRequest(HttpServletRequest request) {
         final  String authHeader = request.getHeader("Authorization");
@@ -21,19 +30,4 @@ public class LogoutService implements LogoutHandler {
                 startsWithIgnoreCase(authHeader, "Bearer ") ?
                 authHeader.substring(7) : null;
     }
-
-//      final String authHeader = request.getHeader("Authorization");
-//        final String jwt;
-//        if(authHeader == null || !authHeader.startsWith("Bearer ")){
-//            return;
-//        }
-//        jwt = authHeader.substring(7);
-//        var storedToken = jwtTokenRepository.findByJwtToken(jwt).orElse(null);
-//        if(storedToken != null){
-//            storedToken.setExpired(true);
-//            storedToken.setRevoked(true);
-//            jwtTokenRepository.save(storedToken);
-//            SecurityContextHolder.clearContext();
-//        }
-//    }
 }
--- Testing/src/main/java/com/test/Testing/security/SecurityConfig.java
+++ Testing/src/main/java/com/test/Testing/security/SecurityConfig.java
@@ -9,11 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,12 +23,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final AuthenticationEntryPoint authenticationEntryPoint;
     private final JwtAuthenticationFilter authenticationFilter;
+    private final LogoutHandler logoutHandler;
     private final String[] AUTHENTICATION_WHITE_LIST = {"/api/v1/testing/student/register", "/api/v1/testing/student/login", "/api/v1/testing/teacher/login"};
     private final String[] SWAGGER = {"/swagger-ui.html", "/swagger-ui/**", "v3/api-docs", "v3/api-docs/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 //                .exceptionHandling()
@@ -39,7 +42,15 @@ public class SecurityConfig {
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
-                .build();
+                .logout(logout -> logout
+                        .logoutUrl("/api/v1/testing/student/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+
+                        .logoutUrl("/api/v1/testing/teacher/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()));
+                return httpSecurity.build();
     }
 
     @Bean
