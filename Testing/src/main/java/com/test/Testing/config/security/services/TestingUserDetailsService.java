81888d2bae80022d5a932800eb6c948f002f1873
David <osodavid001@gmail.com>
2023-08-05 21:07:37 +0100

    testing user details service

--- Testing/src/main/java/com/test/Testing/config/security/services/TestingUserDetailsService.java
+++ Testing/src/main/java/com/test/Testing/config/security/services/TestingUserDetailsService.java
@@ -1,5 +1,24 @@
 package com.test.Testing.config.security.services;
 
-public class TestingUserDetailsService {
+import com.test.Testing.config.security.user.AuthenticatedUser;
+import com.test.Testing.data.model.AppUser;
+import com.test.Testing.data.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TestingUserDetailsService implements UserDetailsService {
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        final AppUser appUser = appUserRepository.findByEmail(email).orElseThrow(
+                ()-> new UsernameNotFoundException("User with this email not found!"));
+        return new AuthenticatedUser(appUser);
+    }
 }
 
