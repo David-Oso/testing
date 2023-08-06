079c4e6c19cedd101bc26720b708341420ca399c
David <osodavid001@gmail.com>
2023-08-06 07:55:37 +0100

    white list

--- Testing/src/main/java/com/test/Testing/config/security/util/WhiteList.java
+++ Testing/src/main/java/com/test/Testing/config/security/util/WhiteList.java
@@ -1,4 +1,29 @@
 package com.test.Testing.config.security.util;
 
+import lombok.Getter;
+
+@Getter
 public class WhiteList {
+    public static  String[] freeAccess(){
+        return new String[]{
+                "/api/v1/testing/student/register",
+                "/api/v1/testing/student/login",
+                "/api/v1/testing/teacher/login"
+        };
+    }
+
+    public static String[] swagger() {
+        return new String[]{
+                "/v2/api-docs",
+                "/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "webjars/**",
+                "/swagger-ui.html"
+        };
+    }
 }
